a094f25261ab14b3da0e0a59570ee4417ac35654
edunsouza <eduardo.souza.ens@gmail.com>
2022-08-16 01:35:50 -0300

    removing prettier and including @react-navigation

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,14 +3,18 @@ module.exports = {
   extends: '@react-native-community',
   parser: '@typescript-eslint/parser',
   plugins: ['@typescript-eslint'],
+  rules: {
+    'prettier/prettier': 0,
+    'comma-dangle': ['error', 'never']
+  },
   overrides: [
     {
       files: ['*.ts', '*.tsx'],
       rules: {
         '@typescript-eslint/no-shadow': ['error'],
         'no-shadow': 'off',
-        'no-undef': 'off',
-      },
-    },
-  ],
+        'no-undef': 'off'
+      }
+    }
+  ]
 };
